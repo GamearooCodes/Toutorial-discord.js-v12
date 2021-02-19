88184ba836b3b1ab333b28ab0330388e6e748557
Gamearoo Codes <gamearoo@animetopic.pro>
2021-02-19 00:13:30 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.token = "NzkzODYwMjY1NTc3MTUyNTIy.X-yZ_A.-ba50qpvHx8dvQZdaG20Qa335Vc";
+
+exports.prefix = "r!";
+
+exports.version = "1.0";
+exports.color = "";
+
+exports.beta = true;
+exports.name = "Rem";
